b537656b4b09717da3255c1ce8121c4becfa9de5
Scott Anderson <ascent12@hotmail.com>
2017-06-05 15:09:41 +1200

    Removed inline function

--- include/common/log.h
+++ include/common/log.h
@@ -14,20 +14,11 @@
 void _wlr_log(log_importance_t verbosity, const char *format, ...) ATTRIB_PRINTF(2, 3);
 void _wlr_vlog(log_importance_t verbosity, const char *format, va_list args) ATTRIB_PRINTF(2, 0);
 
-// Returns a filename relative to wlroots source directory
-static inline const char *_relpath(const char *file) {
-	const char *prefix = WLR_SRC_DIR;
-	if (strncmp(file, prefix, strlen(prefix)) == 0) {
-		file += strlen(prefix) + 1;
-	}
-	return file;
-}
-
 #define wlr_log(verb, fmt, ...) \
-	_wlr_log(verb, "[%s:%d] " fmt, _relpath(__FILE__), __LINE__, ##__VA_ARGS__)
+	_wlr_log(verb, "[%s:%d] " fmt, __FILE__ + strlen(WLR_SRC_DIR) + 1, __LINE__, ##__VA_ARGS__)
 
 #define wlr_vlog(verb, fmt, args) \
-	_wlr_vlog(verb, "[%s:%d] " fmt, _relpath(__FILE__), __LINE__, args)
+	_wlr_vlog(verb, "[%s:%d] " fmt, __FILE__ + strlen(WLR_SRC_DIR) + 1, __LINE__, args)
 
 #define wlr_log_errno(verb, fmt, ...) \
 	wlr_log(verb, fmt ": %s", ##__VA_ARGS__, strerror(errno))
