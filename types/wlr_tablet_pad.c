a289940bff8bcda5cc6aeb1d88f4f1e5c2c38317
Dominique Martinet <asmadeus@codewreck.org>
2017-08-14 15:59:59 +0200

    Refactor out wlr_tablet_pad_state

--- types/wlr_tablet_pad.c
+++ types/wlr_tablet_pad.c
@@ -4,21 +4,19 @@
 #include <wlr/types/wlr_tablet_pad.h>
 #include <wlr/interfaces/wlr_tablet_pad.h>
 
-struct wlr_tablet_pad *wlr_tablet_pad_create(struct wlr_tablet_pad_impl *impl,
-		struct wlr_tablet_pad_state *state) {
-	struct wlr_tablet_pad *pad = calloc(1, sizeof(struct wlr_tablet_pad));
+void wlr_tablet_pad_init(struct wlr_tablet_pad *pad,
+		struct wlr_tablet_pad_impl *impl) {
 	pad->impl = impl;
-	pad->state = state;
 	wl_signal_init(&pad->events.button);
 	wl_signal_init(&pad->events.ring);
 	wl_signal_init(&pad->events.strip);
-	return pad;
 }
 
 void wlr_tablet_pad_destroy(struct wlr_tablet_pad *pad) {
 	if (!pad) return;
-	if (pad->impl) {
-		pad->impl->destroy(pad->state);
+	if (pad->impl && pad->impl->destroy) {
+		pad->impl->destroy(pad);
+	} else {
+		free(pad);
 	}
-	free(pad);
 }
