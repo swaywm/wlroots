7d0bf9a1a77420f09389bda1acafcd4bd42e82f1
nyorain <nyorain@gmail.com>
2017-08-15 12:16:23 +0200

    Simplify tablet pad destruction

--- types/wlr_tablet_pad.c
+++ types/wlr_tablet_pad.c
@@ -13,11 +13,7 @@ void wlr_tablet_pad_init(struct wlr_tablet_pad *pad,
 }
 
 void wlr_tablet_pad_destroy(struct wlr_tablet_pad *pad) {
-	if (!pad) {
-		return;
-	}
-	
-	if (pad->impl && pad->impl->destroy) {
+	if (pad && pad->impl && pad->impl->destroy) {
 		pad->impl->destroy(pad);
 	} else {
 		free(pad);
