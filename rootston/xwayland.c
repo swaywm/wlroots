4d7ff3cb48d1057765d7307081d8387648ab59b1
Versus Void <versusvoid@gmail.com>
2017-10-06 13:57:25 +0300

    Activate only active xwayland views Fix #217

--- rootston/xwayland.c
+++ rootston/xwayland.c
@@ -39,8 +39,10 @@ static void handle_request_configure(struct wl_listener *listener, void *data) {
 }
 
 static void activate(struct roots_view *view, bool active) {
-	wlr_xwayland_surface_activate(view->desktop->xwayland,
-		view->xwayland_surface);
+	if (active) {
+		wlr_xwayland_surface_activate(view->desktop->xwayland,
+			view->xwayland_surface);
+	}
 }
 
 void handle_xwayland_surface(struct wl_listener *listener, void *data) {
