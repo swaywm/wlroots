f77c4cae53504c51a57c24906cce6694b2f9cbad
emersion <contact@emersion.fr>
2017-10-16 22:09:09 +0200

    Minor protocol fixes, add -l option to examples/surface-layer

--- examples/layer-surface.c
+++ examples/layer-surface.c
@@ -1,8 +1,10 @@
-#include <EGL/egl.h>
-#include <GLES2/gl2.h>
+#define _XOPEN_SOURCE
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <unistd.h>
+#include <EGL/egl.h>
+#include <GLES2/gl2.h>
 #include <wayland-client-protocol.h>
 #include <wayland-client.h>
 #include <wayland-egl.h>
@@ -108,7 +110,8 @@ void global_registry_handler(void *data, struct wl_registry *registry,
 		uint32_t id, const char *interface, uint32_t version) {
 	printf("Got a registry event for %s id %d\n", interface, id);
 	if (strcmp(interface, "wl_compositor") == 0) {
-		compositor = wl_registry_bind(registry, id, &wl_compositor_interface, 1);
+		compositor =
+			wl_registry_bind(registry, id, &wl_compositor_interface, 1);
 	} else if (strcmp(interface, "wl_seat") == 0) {
 		seat = wl_registry_bind(registry, id, &wl_seat_interface, 1);
 		wl_seat_add_listener(seat, &seat_listener, NULL);
@@ -151,12 +154,13 @@ static void create_window() {
 	egl_window = wl_egl_window_create(surface, 480, 360);
 	if (egl_window == EGL_NO_SURFACE) {
 		fprintf(stderr, "Can't create egl window\n");
-		exit(1);
+		exit(EXIT_FAILURE);
 	} else {
 		fprintf(stderr, "Created egl window\n");
 	}
 
-	egl_surface = eglCreateWindowSurface(egl_display, egl_conf, (EGLNativeWindowType)egl_window, NULL);
+	egl_surface = eglCreateWindowSurface(egl_display, egl_conf,
+		(EGLNativeWindowType)egl_window, NULL);
 
 	draw();
 
@@ -182,7 +186,7 @@ static void init_egl() {
 	egl_display = eglGetDisplay((EGLNativeDisplayType)display);
 	if (egl_display == EGL_NO_DISPLAY) {
 		fprintf(stderr, "Can't create egl display\n");
-		exit(1);
+		exit(EXIT_FAILURE);
 	} else {
 		fprintf(stderr, "Created egl display\n");
 	}
@@ -190,13 +194,13 @@ static void init_egl() {
 	EGLint major, minor;
 	if (eglInitialize(egl_display, &major, &minor) != EGL_TRUE) {
 		fprintf(stderr, "Can't initialise egl display\n");
-		exit(1);
+		exit(EXIT_FAILURE);
 	}
 	printf("EGL major: %d, minor %d\n", major, minor);
 
 	if (!eglBindAPI(EGL_OPENGL_ES_API)) {
 		fprintf(stderr, "Can't bind API\n");
-		exit(1);
+		exit(EXIT_FAILURE);
 	} else {
 		fprintf(stderr, "Bound API\n");
 	}
@@ -208,7 +212,7 @@ static void init_egl() {
 	EGLConfig *configs = calloc(count, sizeof(EGLConfig));
 	if (configs == NULL) {
 		fprintf(stderr, "Can't allocate EGL configs\n");
-		exit(1);
+		exit(EXIT_FAILURE);
 	}
 
 	EGLint n;
@@ -229,11 +233,43 @@ static void init_egl() {
 		context_attribs);
 }
 
+enum surface_layers_layer parse_layer(const char *s) {
+	if (strcmp(s, "background") == 0) {
+		return SURFACE_LAYERS_LAYER_BACKGROUND;
+	} else if (strcmp(s, "bottom") == 0) {
+		return SURFACE_LAYERS_LAYER_BOTTOM;
+	} else if (strcmp(s, "top") == 0) {
+		return SURFACE_LAYERS_LAYER_TOP;
+	} else if (strcmp(s, "overlay") == 0) {
+		return SURFACE_LAYERS_LAYER_OVERLAY;
+	}
+	fprintf(stderr, "Unknown layer name: %s\n", s);
+	exit(EXIT_FAILURE);
+}
+
 int main(int argc, char **argv) {
+	enum surface_layers_layer layer_index = SURFACE_LAYERS_LAYER_OVERLAY;
+	while (1) {
+		int opt = getopt(argc, argv, "l:h");
+		if (opt == -1) {
+			break;
+		}
+
+		switch (opt) {
+		case 'l':
+			layer_index = parse_layer(optarg);
+			break;
+		case 'h':
+		default:
+			fprintf(stderr, "Usage: %s [-l <layer>] [-h]\n", argv[0]);
+			exit(EXIT_FAILURE);
+		}
+	}
+
 	display = wl_display_connect(NULL);
 	if (display == NULL) {
 		fprintf(stderr, "Can't connect to display\n");
-		exit(1);
+		exit(EXIT_FAILURE);
 	}
 	printf("connected to display\n");
 
@@ -245,30 +281,30 @@ int main(int argc, char **argv) {
 
 	if (compositor == NULL) {
 		fprintf(stderr, "Can't find compositor\n");
-		exit(1);
+		exit(EXIT_FAILURE);
 	}
 	if (output == NULL) {
 		fprintf(stderr, "Can't find output\n");
-		exit(1);
+		exit(EXIT_FAILURE);
 	}
 	if (layers == NULL) {
 		fprintf(stderr, "Can't find surface_layers\n");
-		exit(1);
+		exit(EXIT_FAILURE);
 	}
 
 	surface = wl_compositor_create_surface(compositor);
 	if (surface == NULL) {
 		fprintf(stderr, "Can't create surface\n");
-		exit(1);
+		exit(EXIT_FAILURE);
 	} else {
 		fprintf(stderr, "Created surface %p\n", surface);
 	}
 
 	layer = surface_layers_get_layer_surface(layers, surface, output,
-		SURFACE_LAYERS_LAYER_OVERLAY);
+		layer_index);
 	if (!layer) {
 		fprintf(stderr, "Can't create surface_layer\n");
-		exit(1);
+		exit(EXIT_FAILURE);
 	}
 
 	init_egl();
@@ -282,5 +318,5 @@ int main(int argc, char **argv) {
 	wl_display_disconnect(display);
 	printf("disconnected from display\n");
 
-	exit(0);
+	exit(EXIT_SUCCESS);
 }
