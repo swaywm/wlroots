ab7877c9de79ac4e74e473966fb845d81b3b8c78
Scott Anderson <ascent12@hotmail.com>
2017-12-08 15:28:34 +1300

    Add polygon rendering functions

--- render/shaders.c
+++ render/shaders.c
@@ -1,39 +1,20 @@
 #include <GLES2/gl2.h>
 
-// Colored quads
-const GLchar quad_vert_src[] =
+const GLchar poly_vert_src[] =
 "uniform mat3 proj;\n"
-"uniform vec4 color;\n"
 "attribute vec2 pos;\n"
-"attribute vec2 texcoord;\n"
-"varying vec4 v_color;\n"
-"varying vec2 v_texcoord;\n"
 "void main() {\n"
-"	gl_Position = vec4(proj * vec3(pos, 0.0), 1.0);\n"
-"	v_color = color;\n"
-"	v_texcoord = texcoord;\n"
+"	vec3 new_pos = proj * vec3(pos, 1.0);\n"
+"	gl_Position = vec4(new_pos.xy, 0.0, 1.0);\n"
 "}\n";
 
-const GLchar quad_frag_src[] =
+const GLchar poly_frag_src[] =
 "precision mediump float;\n"
-"varying vec4 v_color;\n"
-"varying vec2 v_texcoord;\n"
+"uniform vec4 color;\n"
 "void main() {\n"
-"	gl_FragColor = v_color;\n"
+"	gl_FragColor = color;\n"
 "}\n";
 
-// Colored ellipses
-const GLchar ellipse_frag_src[] =
-"precision mediump float;\n"
-"varying vec4 v_color;\n"
-"varying vec2 v_texcoord;\n"
-"void main() {\n"
-"	float l = length(v_texcoord - vec2(0.5, 0.5));\n"
-"	if (l > 0.5) discard;\n"
-"	gl_FragColor = v_color;\n"
-"}\n";
-
-// Textured quads
 const GLchar tex_vert_src[] =
 "uniform mat3 proj;\n"
 "attribute vec2 pos;\n"
@@ -45,26 +26,7 @@ const GLchar tex_vert_src[] =
 "	v_texcoord = texcoord;\n"
 "}\n";
 
-const GLchar rgba_frag_src[] =
-"precision mediump float;\n"
-"varying vec2 v_texcoord;\n"
-"uniform sampler2D tex;\n"
-"uniform float alpha;\n"
-"void main() {\n"
-"	gl_FragColor = alpha * texture2D(tex, v_texcoord);\n"
-"}\n";
-
-const GLchar rgbx_frag_src[] =
-"precision mediump float;\n"
-"varying vec2 v_texcoord;\n"
-"uniform sampler2D tex;\n"
-"uniform float alpha;\n"
-"void main() {\n"
-"	gl_FragColor.rgb = alpha * texture2D(tex, v_texcoord).rgb;\n"
-"	gl_FragColor.a = alpha;\n"
-"}";
-
-const GLchar extn_frag_src[] =
+const GLchar tex_frag_src[] =
 "#extension GL_OES_EGL_image_external : require\n"
 "precision mediump float;\n"
 "varying vec2 v_texcoord;\n"
