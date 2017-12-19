dabfe159dea502a51617d90785343cee18cf408b
Dominique Martinet <asmadeus@codewreck.org>
2017-12-19 19:55:18 +0100

    wl backend: add wlr_output_is_wl

--- include/backend/wayland.h
+++ include/backend/wayland.h
@@ -8,7 +8,6 @@
 #include <wlr/render/egl.h>
 #include <wlr/backend/wayland.h>
 #include <wlr/types/wlr_box.h>
-#include <wlr/types/wlr_output.h>
 #include <wlr/types/wlr_input_device.h>
 #include <wayland-util.h>
 
--- include/wlr/backend/wayland.h
+++ include/wlr/backend/wayland.h
@@ -4,6 +4,7 @@
 #include <wayland-client.h>
 #include <wayland-server.h>
 #include <wlr/backend.h>
+#include <wlr/types/wlr_output.h>
 #include <stdbool.h>
 
 /**
@@ -11,6 +12,7 @@
  * you must use wlr_wl_output_create to add them.
  */
 struct wlr_backend *wlr_wl_backend_create(struct wl_display *display);
+
 /**
  * Adds a new output to this backend. You may remove outputs by destroying them.
  * Note that if called before initializing the backend, this will return NULL
@@ -18,9 +20,15 @@ struct wlr_backend *wlr_wl_backend_create(struct wl_display *display);
  * the output_add signal).
  */
 struct wlr_output *wlr_wl_output_create(struct wlr_backend *backend);
+
 /**
  * True if the given backend is a wlr_wl_backend.
  */
 bool wlr_backend_is_wl(struct wlr_backend *backend);
+
+/**
+ * True if the given output is a wlr_wl_backend_output.
+ */
+bool wlr_output_is_wl(struct wlr_output *output);
 
 #endif
