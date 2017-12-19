fcd7062542bec6f9ab247dbc63c6eead457006bf
Dominique Martinet <asmadeus@codewreck.org>
2017-12-19 20:40:36 +0100

    x11 backend: add wlr_input_device_is_x11

--- include/wlr/backend/x11.h
+++ include/wlr/backend/x11.h
@@ -4,12 +4,14 @@
 #include <stdbool.h>
 #include <wayland-server.h>
 #include <wlr/backend.h>
+#include <wlr/types/wlr_input_device.h>
 #include <wlr/types/wlr_output.h>
 
 struct wlr_backend *wlr_x11_backend_create(struct wl_display *display,
 	const char *x11_display);
 
 bool wlr_backend_is_x11(struct wlr_backend *backend);
+bool wlr_input_device_is_x11(struct wlr_input_device *device);
 bool wlr_output_is_x11(struct wlr_output *output);
 
 #endif
