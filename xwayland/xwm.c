6ed1884c84f470707ed5e6b56a92d86b51034625
Dominique Martinet <asmadeus@codewreck.org>
2017-12-29 21:23:55 +0100

    xwm_get_render_format: fix typo in error message

--- xwayland/xwm.c
+++ xwayland/xwm.c
@@ -1255,7 +1255,7 @@ static void xwm_get_render_format(struct wlr_xwm *xwm) {
 	xcb_render_query_pict_formats_reply_t *reply =
 		xcb_render_query_pict_formats_reply(xwm->xcb_conn, cookie, NULL);
 	if (!reply) {
-		wlr_log(L_ERROR, "Did not get any reply from xcb_rrender_query_pict_formats");
+		wlr_log(L_ERROR, "Did not get any reply from xcb_render_query_pict_formats");
 		return;
 	}
 	xcb_render_pictforminfo_iterator_t iter =
