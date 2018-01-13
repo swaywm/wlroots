dfae5ff98f15ad65472cb2d39a8153ced14be1b8
Markus Ongyerth <ongy@ongy.net>
2018-01-13 21:41:04 +0100

    Update comment for wlr_headless_backend_create

--- include/wlr/backend/wayland.h
+++ include/wlr/backend/wayland.h
@@ -11,6 +11,10 @@
 /**
  * Creates a new wlr_wl_backend. This backend will be created with no outputs;
  * you must use wlr_wl_output_create to add them.
+ *
+ * The `remote` argument is the name of the host compositor wayland socket. Set
+ * to NULL for the default behaviour (WAYLAND_DISPLAY env variable or wayland-0
+ * default)
  */
 struct wlr_backend *wlr_wl_backend_create(struct wl_display *display, const char *remote);
 
