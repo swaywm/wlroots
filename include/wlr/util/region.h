a98ece68d32031dbd81b0064fad659515fc09f76
emersion <contact@emersion.fr>
2018-01-25 21:54:51 +0100

    util: add docs for wlr_region_scale

--- include/wlr/util/region.h
+++ include/wlr/util/region.h
@@ -3,6 +3,12 @@
 
 #include <pixman.h>
 
+/**
+ * Scales a region, ie. multiplies all its coordinates by `scale`.
+ *
+ * The resulting coordinates are rounded up or down so that the new region is
+ * at least as big as the original one.
+ */
 void wlr_region_scale(pixman_region32_t *dst, pixman_region32_t *src,
 	float scale);
 
