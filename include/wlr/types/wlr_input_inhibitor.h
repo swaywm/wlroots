ef4e833f13e69dced02a169225528f548c98b3f8
Drew DeVault <sir@cmpwn.com>
2018-04-03 10:50:16 -0400

    Add wlr_input_inhibitor

---
+++ include/wlr/types/wlr_input_inhibitor.h
@@ -0,0 +1,25 @@
+#ifndef WLR_TYPES_INPUT_INHIBITOR_H
+#define WLR_TYPES_INPUT_INHIBITOR_H
+#include <wayland-server.h>
+
+struct wlr_input_inhibit_manager {
+	struct wl_global *wl_global;
+	struct wl_client *active_client;
+	struct wl_resource *active_inhibitor;
+
+	struct wl_listener display_destroy;
+
+	struct {
+		struct wl_signal activate;   // struct wlr_input_inhibit_manager *
+		struct wl_signal deactivate; // struct wlr_input_inhibit_manager *
+	} events;
+
+	void *data;
+};
+
+struct wlr_input_inhibit_manager *wlr_input_inhibit_manager_create(
+		struct wl_display *display);
+void wlr_input_inhibit_manager_destroy(
+		struct wlr_input_inhibit_manager *manager);
+
+#endif
