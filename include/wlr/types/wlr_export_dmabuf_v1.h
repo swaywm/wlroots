36bd4795d4fe2282dfcc59f26863bac2896a4a3f
emersion <contact@emersion.fr>
2018-05-22 17:38:05 +0100

    export-dmabuf: add basic and incomplete implementation

---
+++ include/wlr/types/wlr_export_dmabuf_v1.h
@@ -0,0 +1,33 @@
+#ifndef WLR_TYPES_WLR_EXPORT_DMABUF_V1_H
+#define WLR_TYPES_WLR_EXPORT_DMABUF_V1_H
+
+#include <wayland-server.h>
+
+struct wlr_export_dmabuf_manager_v1;
+
+struct wlr_export_dmabuf_frame_v1 {
+	struct wl_resource *resource;
+	struct wlr_export_dmabuf_manager_v1 *manager;
+	struct wl_list link;
+
+	struct wlr_output *output;
+};
+
+struct wlr_export_dmabuf_manager_v1 {
+	struct wl_global *global;
+	struct wl_list resources;
+	struct wl_list frames;
+
+	struct wl_listener display_destroy;
+
+	struct {
+		struct wl_signal destroy;
+	} events;
+};
+
+struct wlr_export_dmabuf_manager_v1 *wlr_export_dmabuf_manager_v1_create(
+	struct wl_display *display);
+void wlr_export_dmabuf_manager_v1_destroy(
+	struct wlr_export_dmabuf_manager_v1 *manager);
+
+#endif
