b25e230df828ded199d5648380a273ebbd3c5607
sghctoma <sghctoma@gmail.com>
2018-11-08 13:48:21 +0100

    Force ftruncate for shared mem. objects on FreeBSD
    FreeBSD does not allow to use posix_fallocate on shared memory objects.

--- util/shm.c
+++ util/shm.c
@@ -42,7 +42,7 @@ int allocate_shm_file(size_t size) {
 		return -1;
 	}
 
-#ifdef WLR_HAS_POSIX_FALLOCATE
+#if defined(WLR_HAS_POSIX_FALLOCATE) && !defined(__FreeBSD__)
 	int ret;
 	do {
 		ret = posix_fallocate(fd, 0, size);
