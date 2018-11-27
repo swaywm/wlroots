e7fba556a8237cb048fa157f5ec068fa20d80a55
Philipp Ludwig <git-devel@philippludwig.net>
2018-11-27 23:19:26 +0100

    xcursor: fix crash when encountering cursor themes with circular dependencies
    Port of https://cgit.freedesktop.org/xorg/lib/libXcursor/commit/src?id=f64a8cc1a65dcad4294e2988b402a34175019663

--- xcursor/xcursor.c
+++ xcursor/xcursor.c
@@ -839,7 +839,12 @@ XcursorScanTheme (const char *theme, const char *name)
      * Recurse to scan inherited themes
      */
     for (i = inherits; i && f == NULL; i = _XcursorNextPath (i))
-	f = XcursorScanTheme (i, name);
+    {
+        if (strcmp(i, theme) != 0)
+            f = XcursorScanTheme (i, name);
+        else
+            printf("Not calling XcursorScanTheme because of circular dependency: %s. %s", i, name);
+    }
     if (inherits != NULL)
 	free (inherits);
     return f;
