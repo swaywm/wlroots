f8129ecbc5147a62d93d6bac5e61e5ce79d89667
Ryan Walklin <ryan@testtoast.com>
2018-12-15 14:59:51 +1100

    Remove unused variables

--- rootston/switch.c
+++ rootston/switch.c
@@ -13,8 +13,6 @@ void roots_switch_handle_toggle(struct roots_switch *lid_switch,
     struct wl_list *bound_switches = &lid_switch->seat->input->server->config->switches;
     struct roots_switch_config *sc;
     wl_list_for_each(sc, bound_switches, link) {
-        bool device_match = false;
-        bool state_match = false;
         if ((sc->name != NULL && strcmp(event->device->name, sc->name) != 0) &&
                 (sc->name == NULL && event->switch_type != sc->switch_type)) {
             continue;
