c01b81c99c9e7d6418611bab4fd709c4c2a8b3d9
Scott Anderson <ascent12@hotmail.com>
2019-04-01 19:15:56 +0300

    render: introduce wlr_drm_format_set
    This types adds a container for formats + modifiers.
    
    A list that is of [format [modifier]] was chosen instead of
    [format modifer] because that is how GBM accepts them.
    
    Co-Authored-By: emersion <contact@emersion.fr>

---
+++ include/wlr/render/drm_format_set.h
@@ -0,0 +1,27 @@
+#ifndef WLR_RENDER_DRM_FORMAT_SET_H
+#define WLR_RENDER_DRM_FORMAT_SET_H
+
+#include <stdbool.h>
+#include <stddef.h>
+#include <stdint.h>
+
+struct wlr_drm_format {
+	uint32_t format;
+	size_t len, cap;
+	uint64_t modifiers[];
+};
+
+struct wlr_drm_format_set {
+	size_t len, cap;
+	struct wlr_drm_format **formats;
+};
+
+void wlr_drm_format_set_finish(struct wlr_drm_format_set *set);
+
+const struct wlr_drm_format *wlr_drm_format_set_get(
+	const struct wlr_drm_format_set *set, uint32_t format);
+
+bool wlr_drm_format_set_add(struct wlr_drm_format_set *set, uint32_t format,
+	uint64_t modifier);
+
+#endif
