25737a3ac2e483a8d20c99ab00050c4705ff2b41
Simon Ser <contact@emersion.fr>
2019-04-29 22:32:35 +0300

    render/dmabuf: add wlr_dmabuf_attributes_copy

--- render/dmabuf.c
+++ render/dmabuf.c
@@ -1,10 +1,28 @@
+#define _POSIX_C_SOURCE 200809L
+#include <fcntl.h>
 #include <unistd.h>
 #include <wlr/render/dmabuf.h>
+#include <wlr/util/log.h>
 
-void wlr_dmabuf_attributes_finish( struct wlr_dmabuf_attributes *attribs) {
+void wlr_dmabuf_attributes_finish(struct wlr_dmabuf_attributes *attribs) {
 	for (int i = 0; i < attribs->n_planes; ++i) {
 		close(attribs->fd[i]);
 		attribs->fd[i] = -1;
 	}
 	attribs->n_planes = 0;
+}
+
+bool wlr_dmabuf_attributes_copy(struct wlr_dmabuf_attributes *dst,
+		struct wlr_dmabuf_attributes *src) {
+	memcpy(dst, src, sizeof(struct wlr_dmabuf_attributes));
+
+	for (int i = 0; i < src->n_planes; ++i) {
+		dst->fd[i] = fcntl(src->fd[i], F_DUPFD_CLOEXEC, 0);
+		if (dst->fd[i] < 0) {
+			wlr_log_errno(WLR_ERROR, "fcntl(F_DUPFD_CLOEXEC) failed");
+			return false;
+		}
+	}
+
+	return true;
 }
