58b2584863201f32d1a4770a1d5cde5e57b300c6
Drew DeVault <sir@cmpwn.com>
2019-08-09 08:34:59 +0900

    Remove rootston

--- include/rootston/bindings.h
+++
@@ -1,9 +0,0 @@
-#ifndef ROOTSTON_BINDINGS_H
-#define ROOTSTON_BINDINGS_H
-
-#include "rootston/seat.h"
-#include "rootston/input.h"
-
-void execute_binding_command(struct roots_seat *seat, struct roots_input *input, const char *command);
-
-#endif //ROOTSTON_BINDINGS_H
--- include/rootston/virtual_keyboard.h
+++
@@ -1,7 +0,0 @@
-#ifndef ROOTSTON_VIRTUAL_KEYBOARD_H
-#define ROOTSTON_VIRTUAL_KEYBOARD_H
-
-#include <wayland-server-core.h>
-
-void handle_virtual_keyboard(struct wl_listener *listener, void *data);
-#endif
