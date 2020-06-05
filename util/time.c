c9c31f803eaebf69481d24ac9355cd1f8d3111d9
Tudor Brindus <me@tbrindus.ca>
2020-06-05 17:50:00 -0400

    util/time: de-duplicate `timespec_to_msec`

--- util/time.c
+++ util/time.c
@@ -4,8 +4,12 @@
 
 #include "util/time.h"
 
+int64_t timespec_to_msec(const struct timespec *a) {
+	return (int64_t)a->tv_sec * 1000 + a->tv_nsec / 1000000;
+}
+
 uint32_t get_current_time_msec(void) {
 	struct timespec now;
 	clock_gettime(CLOCK_MONOTONIC, &now);
-	return now.tv_sec * 1000 + now.tv_nsec / 1000000;
+	return timespec_to_msec(&now);
 }
