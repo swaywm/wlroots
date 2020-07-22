caacc7c47ad03cc7819b821dfc0cb802f3998739
DerVerruckteFuchs <derverrucktefuchs@protonmail.ch>
2020-07-21 21:27:04 -0400

    collapse first three if blocks into one

--- backend/drm/drm.c
+++ backend/drm/drm.c
@@ -109,14 +109,9 @@ static bool add_plane(struct wlr_drm_backend *drm,
 	for (size_t j = 0; j < drm_plane->count_formats; ++j) {
 		uint32_t fmt = drm_plane->formats[j];
 
-		if (fmt == DRM_FORMAT_ARGB2101010) {
+		if (fmt == DRM_FORMAT_ARGB2101010 || DRM_FORMAT_XRGB2101010 ||
+				DRM_FORMAT_ARGB8888) {
 			// Prefer formats with 10 bit color and/or alpha channel.
-			rgb_format = fmt;
-			break;
-		} else if (fmt == DRM_FORMAT_XRGB2101010) {
-			rgb_format = fmt;
-			break;
-		} else if (fmt == DRM_FORMAT_ARGB8888) {
 			rgb_format = fmt;
 			break;
 		} else if (fmt == DRM_FORMAT_XRGB8888) {
