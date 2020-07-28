26af316b3b3c15c0c2cbc76f2d398e5c3ac70c64
Simon Ser <contact@emersion.fr>
2020-07-28 13:45:24 +0200

    render/gles2: make wlr_gles2_texture_from_* private
    These functions are unused by compositors (see e.g. [1]) and prevent
    wlr_gles2_texture from accessing wlr_gles2_renderer state. This is an
    issue for proper teardown [2] and for accessing GLES2 extensions.
    
    [1]: https://github.com/swaywm/wlroots/pull/1962#issuecomment-569511830
    [2]: https://github.com/swaywm/wlroots/pull/1962

--- include/wlr/render/gles2.h
+++ include/wlr/render/gles2.h
@@ -21,14 +21,6 @@ struct wlr_egl *wlr_gles2_renderer_get_egl(struct wlr_renderer *renderer);
 bool wlr_gles2_renderer_check_ext(struct wlr_renderer *renderer,
 	const char *ext);
 
-struct wlr_texture *wlr_gles2_texture_from_pixels(struct wlr_egl *egl,
-	enum wl_shm_format wl_fmt, uint32_t stride, uint32_t width, uint32_t height,
-	const void *data);
-struct wlr_texture *wlr_gles2_texture_from_wl_drm(struct wlr_egl *egl,
-	struct wl_resource *data);
-struct wlr_texture *wlr_gles2_texture_from_dmabuf(struct wlr_egl *egl,
-	struct wlr_dmabuf_attributes *attribs);
-
 struct wlr_gles2_texture_attribs {
 	GLenum target; /* either GL_TEXTURE_2D or GL_TEXTURE_EXTERNAL_OES */
 	GLuint tex;
