7c6212a0f71eeab23a4ff4241cb266ec219fad2d
Simon Ser <contact@emersion.fr>
2020-08-07 19:02:49 +0200

    render/drm_format_set: introduce wlr_drm_format_dup

---
+++ include/render/drm_format_set.h
@@ -0,0 +1,8 @@
+#ifndef RENDER_DRM_FORMAT_SET_H
+#define RENDER_DRM_FORMAT_SET_H
+
+#include <wlr/render/drm_format_set.h>
+
+struct wlr_drm_format *wlr_drm_format_dup(const struct wlr_drm_format *format);
+
+#endif
