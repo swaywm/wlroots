6f4764fdd4dc2d1c9249bbd862a95ce2fd1dd0e4
Simon Ser <contact@emersion.fr>
2020-12-13 12:53:43 +0100

    util/time: add timespec_from_nsec

--- include/util/time.h
+++ include/util/time.h
@@ -14,6 +14,11 @@ uint32_t get_current_time_msec(void);
 int64_t timespec_to_msec(const struct timespec *a);
 
 /**
+ * Convert nanoseconds to a timespec.
+ */
+void timespec_from_nsec(struct timespec *r, int64_t nsec);
+
+/**
  * Subtracts timespec `b` from timespec `a`, and stores the difference in `r`.
  */
 void timespec_sub(struct timespec *r, const struct timespec *a,
