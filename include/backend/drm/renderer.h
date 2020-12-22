5bd86b94f9a6f3b41d1bc4bbf2985632fdd6d0a7
Simon Ser <contact@emersion.fr>
2020-12-22 18:08:10 +0100

    backend/drm: add wlr_drm_buf.local_wlr_buf

--- include/backend/drm/renderer.h
+++ include/backend/drm/renderer.h
@@ -30,10 +30,9 @@ struct wlr_drm_surface {
 };
 
 struct wlr_drm_fb {
-	struct wlr_buffer *wlr_buf;
+	struct wlr_buffer *wlr_buf; // original buffer
 
-	struct wlr_buffer *mgpu_wlr_buf;
-
+	struct wlr_buffer *local_wlr_buf; // GPU-local buffer
 	struct gbm_bo *bo;
 	uint32_t id;
 };
