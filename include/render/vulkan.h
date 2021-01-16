0c0264b27fbd4f329fea4bd5587bc8c26671e4dd
Simon Ser <contact@emersion.fr>
2021-01-16 15:10:26 +0100

    HACK: add VK_EXT_physical_device_drm definitions

--- include/render/vulkan.h
+++ include/render/vulkan.h
@@ -5,6 +5,22 @@
 #include <wlr/types/wlr_buffer.h>
 #include "render/allocator.h"
 
+#define VK_STRUCTURE_TYPE_PHYSICAL_DEVICE_DRM_PROPERTIES_EXT 1000353000
+
+#define VK_EXT_physical_device_drm 1
+#define VK_EXT_PHYSICAL_DEVICE_DRM_SPEC_VERSION 1
+#define VK_EXT_PHYSICAL_DEVICE_DRM_EXTENSION_NAME "VK_EXT_physical_device_drm"
+typedef struct VkPhysicalDeviceDrmPropertiesEXT {
+    VkStructureType    sType;
+    void*              pNext;
+    VkBool32           hasPrimary;
+    VkBool32           hasRender;
+    int64_t            primaryMajor;
+    int64_t            primaryMinor;
+    int64_t            renderMajor;
+    int64_t            renderMinor;
+} VkPhysicalDeviceDrmPropertiesEXT;
+
 struct wlr_vulkan_buffer {
 	struct wlr_buffer base;
 	struct wlr_vulkan_allocator *alloc;
