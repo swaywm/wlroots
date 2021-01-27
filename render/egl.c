swaywm/wlroots

commit 6becc69ec90c2300d974e653ca50d2d33cf94c00

render/egl: fail to create wlr_egl on software renderer
The creation of `wlr_egl` will fail is the device extension
EGL_MESA_device_software is defined. The creation process is allowed to
continue only if the environment variable `WLR_RENDERER_ALLOW_SOFTWARE`
is defined to the value 1.

render/egl.c
@@ -272,6 +272,18 @@ struct wlr_egl *wlr_egl_create(EGLenum platform, void *remote_display,
 			goto error;
 		}
 
+		if (check_egl_ext(device_exts_str, "EGL_MESA_device_software")) {
+			const char *allow_software = getenv("WLR_RENDERER_ALLOW_SOFTWARE");
+			if (strcmp(allow_software, "1") == 0) {
+				wlr_log(WLR_INFO, "Using software rendering");
+			} else {
+				wlr_log(WLR_ERROR, "Software rendering detected, please use "
+						"the WLR_RENDERER_ALLOW_SOFTWARE environment variable "
+						"to proceed");
+				goto error;
+			}
+		}
+
 		egl->exts.device_drm_ext =
 			check_egl_ext(device_exts_str, "EGL_EXT_device_drm");
 	}

commit a406f1947978e5db32615aeea095eab6a5bc19b1

render/egl: fix NULL dereference when using llvmpipe
Fixes: 6becc69ec90c ("render/egl: fail to create wlr_egl on software renderer")

render/egl.c
@@ -274,7 +274,7 @@ struct wlr_egl *wlr_egl_create(EGLenum platform, void *remote_display,
 
 		if (check_egl_ext(device_exts_str, "EGL_MESA_device_software")) {
 			const char *allow_software = getenv("WLR_RENDERER_ALLOW_SOFTWARE");
-			if (strcmp(allow_software, "1") == 0) {
+			if (allow_software != NULL && strcmp(allow_software, "1") == 0) {
 				wlr_log(WLR_INFO, "Using software rendering");
 			} else {
 				wlr_log(WLR_ERROR, "Software rendering detected, please use "