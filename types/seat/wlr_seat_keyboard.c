78befa59f96bb0a13e9ffeb4df1272738c6b5012
Isaac Freund <ifreund@ifreund.xyz>
2021-04-08 09:42:11 +0200

    gtk-primary-selection: drop support
    The standard primary-selection protocol is now widely supported.

--- types/seat/wlr_seat_keyboard.c
+++ types/seat/wlr_seat_keyboard.c
@@ -7,7 +7,6 @@
 #include <unistd.h>
 #include <wayland-server-core.h>
 #include <wlr/types/wlr_data_device.h>
-#include <wlr/types/wlr_gtk_primary_selection.h>
 #include <wlr/types/wlr_input_device.h>
 #include <wlr/util/log.h>
 #include "types/wlr_data_device.h"
