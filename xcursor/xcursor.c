4bbed8b999da0e25833208351a8d4081c1447ece
Yuya Nishihara <yuya@tcha.org>
2021-04-10 15:31:06 +0900

    xcursor: use memcpy() to append string of known size
    Since len <= strlen(elt) is known, we don't need a str*() function. Let's
    simply do memcpy() to suppress linter false positive.
    
    Fixes #2777.

--- xcursor/xcursor.c
+++ xcursor/xcursor.c
@@ -655,11 +655,8 @@ _XcursorAddPathElt (char *path, const char *elt, int len)
 	elt++;
 	len--;
     }
-#pragma GCC diagnostic push
-#pragma GCC diagnostic ignored "-Wstringop-truncation"
-    strncpy (path + pathlen, elt, len);
+    memcpy (path + pathlen, elt, len);
     path[pathlen + len] = '\0';
-#pragma GCC diagnostic pop
 }
 
 static char *
