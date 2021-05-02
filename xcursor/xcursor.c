5f4d3878e17a5e5c3e3506b3cf10abc4bf3ea5ab
Tobias Stoeckmann <tobias@stoeckmann.org>
2021-05-02 16:48:21 +0200

    xcursor: fix CVE-2013-2003
    The libXcursor fix for CVE-2013-2003 has never been imported into
    wlroots, leaving it vulnerable to it.
    
    Changing the argument type to an unsigned type is an effective merge of
    Ilja Van Sprundel's commit in libXcursor.
    
    Proof of Concept (compile with address sanitizer):
    
    $ mkdir -p ~/.local/share/icons/poc/cursors
    $ base64 -d <<< WGN1chAAAAAAAAAA/////w== > \
        ~/.local/share/icons/poc/cursors/poc
    $ echo "seat seat0 xcursor_theme poc 10" > ~/poc-config
    $ sway -c ~/poc-config

--- xcursor/xcursor.c
+++ xcursor/xcursor.c
@@ -301,7 +301,7 @@ _XcursorFileHeaderDestroy (XcursorFileHeader *fileHeader)
 }
 
 static XcursorFileHeader *
-_XcursorFileHeaderCreate (int ntoc)
+_XcursorFileHeaderCreate (XcursorUInt ntoc)
 {
     XcursorFileHeader	*fileHeader;
 
