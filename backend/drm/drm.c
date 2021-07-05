swaywm/wlroots

commit 6c3d080e25e56404228ad7704eed43e40fa0c623

backend/drm: populate cursor plane's current_fb
The set_cursor() hook is a little bit special: it's not really
synchronized to commit() or test(). Once set_cursor() returns true,
the new cursor is part of the current state.

This fixes a state where wlr_drm_connector.cursor_enabled is true
but there is no FB available. This is triggered by set_cursor()
followed by a failed commit(), which resets pending_fb.

We should definitely fix the output interface to make the cursor part
of the pending state, but that's a more involved change.

backend/drm/drm.c
@@ -920,7 +920,7 @@ static bool drm_connector_set_cursor(struct wlr_output *output,
 			local_buf = wlr_buffer_lock(buffer);
 		}
 
-		bool ok = drm_fb_import(&plane->pending_fb, drm, local_buf,
+		bool ok = drm_fb_import(&plane->current_fb, drm, local_buf,
 			&plane->formats);
 		wlr_buffer_unlock(local_buf);
 		if (!ok) {

commit fd21b0d4328fb4f4cc0aafe1ec8b2f3d6125011f

backend/drm: don't clear pending cursor FB on failed commit
The previous fix tried to side-step cursor->pending_fb completely.
However that messes up our buffer locking mechanism.

Instead, stop clearing the pending cursor FB on a failed commit. The
pending cursor FB will remain for the next commit.

Fixes: 6c3d080e25e5 ("backend/drm: populate cursor plane's current_fb")

backend/drm/drm.c
@@ -355,9 +355,12 @@ static bool drm_crtc_commit(struct wlr_drm_connector *conn,
 		}
 	} else {
 		drm_fb_clear(&crtc->primary->pending_fb);
-		if (crtc->cursor != NULL) {
-			drm_fb_clear(&crtc->cursor->pending_fb);
-		}
+		// The set_cursor() hook is a bit special: it's not really synchronized
+		// to commit() or test(). Once set_cursor() returns true, the new
+		// cursor is effectively committed. So don't roll it back here, or we
+		// risk ending up in a state where we don't have a cursor FB but
+		// wlr_drm_connector.cursor_enabled is true.
+		// TODO: fix our output interface to avoid this issue.
 	}
 	return ok;
 }