swaywm/wlroots

commit 82af6e720870e66a49c5e514468ffea71742b280

region: stabilize interface
References: https://github.com/swaywm/wlroots/issues/1008

include/wlr/types/wlr_region.h
@@ -1,17 +1,31 @@
 /*
- * This an unstable interface of wlroots. No guarantees are made regarding the
- * future consistency of this API.
+ * This is a stable interface of wlroots. Future changes will be limited to:
+ *
+ * - New functions
+ * - New struct members
+ * - New enum members
+ *
+ * Note that wlroots does not make an ABI compatibility promise - in the future,
+ * the layout and size of structs used by wlroots may change, requiring code
+ * depending on this header to be recompiled (but not edited).
+ *
+ * Breaking changes are announced by email and follow a 1-year deprecation
+ * schedule. Send an email to ~sircmpwn/wlroots-announce+subscribe@lists.sr.ht
+ * to receive these announcements.
  */
-#ifndef WLR_USE_UNSTABLE
-#error "Add -DWLR_USE_UNSTABLE to enable unstable wlroots features"
-#endif
 
 #ifndef WLR_TYPES_WLR_REGION_H
 #define WLR_TYPES_WLR_REGION_H
 
 #include <pixman.h>
-#include <wayland-server-protocol.h>
 
+struct wl_resource;
+
+/**
+ * Obtain a Pixman region from a wl_region resource.
+ *
+ * To allow clients to create wl_region objects, call wlr_compositor_create().
+ */
 pixman_region32_t *wlr_region_from_resource(struct wl_resource *resource);
 
 #endif

types/wlr_region.c
@@ -2,7 +2,7 @@
 #include <pixman.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <wayland-server-core.h>
+#include <wayland-server-protocol.h>
 #include <wlr/types/wlr_region.h>
 #include "types/wlr_region.h"
 

commit cb00ab0ee267356dc58b53926f028968d902160a

region: drop leftover reference to the mailing list
As per [1], the mailing list isn't used anymore.

[1]: https://github.com/swaywm/wlroots/pull/3016

Fixes: 82af6e720870 ("region: stabilize interface")

include/wlr/types/wlr_region.h
@@ -9,9 +9,8 @@
  * the layout and size of structs used by wlroots may change, requiring code
  * depending on this header to be recompiled (but not edited).
  *
- * Breaking changes are announced by email and follow a 1-year deprecation
- * schedule. Send an email to ~sircmpwn/wlroots-announce+subscribe@lists.sr.ht
- * to receive these announcements.
+ * Breaking changes are announced in the release notes and follow a 1-year
+ * deprecation schedule.
  */
 
 #ifndef WLR_TYPES_WLR_REGION_H