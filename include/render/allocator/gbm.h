ee6c841d47e4ec50835ed5357fc812638b8c0454
Simon Ser <contact@emersion.fr>
2021-09-01 21:34:34 +0200

    render/allocator/gbm: fix create() docs for FD ownership
    Fixes: d9d8fc1ab9b7 ("render/allocator: re-open GBM FD")

--- include/render/allocator/gbm.h
+++ include/render/allocator/gbm.h
@@ -27,7 +27,7 @@ struct wlr_gbm_allocator {
 /**
  * Creates a new GBM allocator from a DRM FD.
  *
- * Does not take ownership over the FD.
+ * Takes ownership over the FD.
  */
 struct wlr_allocator *wlr_gbm_allocator_create(int drm_fd);
 
