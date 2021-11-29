swaywm/wlroots

commit d37eb5c2eaedcada7dd39da1a31c6e9a39c8ed46

linux-dmabuf-v1: filter out LINEAR if implicit
If only INVALID and LINEAR are valid modifiers, we need to filter out
LINEAR since Xwayland won't be able to allocate a BO with the explicit
linear modifier on hardware that does not support explicit modifiers.
The addition of LINEAR is an internal implementation detail which
simplifies the wlroots architecture for now.

Evntually Xwayland should be fixed to filter out modifiers that are not
supported by the GBM implementation, see [1]. This could be done by
querying EGL for the supported modifiers.

[1]: https://gitlab.freedesktop.org/xorg/xserver/-/issues/1166

types/wlr_linux_dmabuf_v1.c
@@ -434,6 +434,16 @@ static void linux_dmabuf_send_modifiers(struct wl_resource *resource,
 		return;
 	}
 
+	// In case only INVALID and LINEAR are advertised, send INVALID only due to XWayland:
+	// https://gitlab.freedesktop.org/xorg/xserver/-/issues/1166
+	if (fmt->len == 2 && wlr_drm_format_has(fmt, DRM_FORMAT_MOD_INVALID)
+			&& wlr_drm_format_has(fmt, DRM_FORMAT_MOD_INVALID)) {
+		uint64_t mod = DRM_FORMAT_MOD_INVALID;
+		zwp_linux_dmabuf_v1_send_modifier(resource, fmt->format,
+			mod >> 32, mod & 0xFFFFFFFF);
+		return;
+	}
+
 	for (size_t i = 0; i < fmt->len; i++) {
 		uint64_t mod = fmt->modifiers[i];
 		zwp_linux_dmabuf_v1_send_modifier(resource, fmt->format,

commit 6bfb930aa70ceb41455b9da2719a10538493a65d

linux-dmabuf-v1: fix implicit check
The implicit check to filter out LINEAR for dmabuf checked for INVALID
twice instead of checking for INVALID & LINEAR. Fix this.

Fixes: d37eb5c2eaed ("linux-dmabuf-v1: filter out LINEAR if implicit")
Reported-by: Dawid Czeluśniak <czelusniakdawid@gmail.com>

types/wlr_linux_dmabuf_v1.c
@@ -437,7 +437,7 @@ static void linux_dmabuf_send_modifiers(struct wl_resource *resource,
 	// In case only INVALID and LINEAR are advertised, send INVALID only due to XWayland:
 	// https://gitlab.freedesktop.org/xorg/xserver/-/issues/1166
 	if (fmt->len == 2 && wlr_drm_format_has(fmt, DRM_FORMAT_MOD_INVALID)
-			&& wlr_drm_format_has(fmt, DRM_FORMAT_MOD_INVALID)) {
+			&& wlr_drm_format_has(fmt, DRM_FORMAT_MOD_LINEAR)) {
 		uint64_t mod = DRM_FORMAT_MOD_INVALID;
 		zwp_linux_dmabuf_v1_send_modifier(resource, fmt->format,
 			mod >> 32, mod & 0xFFFFFFFF);