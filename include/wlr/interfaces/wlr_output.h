e93435016e0dfad0fefed3b68a9b60fe453c723e
Simon Ser <contact@emersion.fr>
2021-11-30 20:33:36 +0100

    output: fix typo in wlr_output_impl.get_primary_formats docs
    The buffer capabilities indicate whether the formats returned are
    for DMA-BUFs or shared memory buffers.

--- include/wlr/interfaces/wlr_output.h
+++ include/wlr/interfaces/wlr_output.h
@@ -86,7 +86,7 @@ struct wlr_output_impl {
 	 */
 	void (*get_cursor_size)(struct wlr_output *output, int *width, int *height);
 	/**
-	 * Get the list of DMA-BUF formats suitable for the primary buffer,
+	 * Get the list of DRM formats suitable for the primary buffer,
 	 * assuming a buffer with the specified capabilities.
 	 *
 	 * If unimplemented, the primary buffer has no format constraint. If NULL
